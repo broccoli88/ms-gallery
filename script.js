d640db7de1edf6ea1fceb3138e25d31cf56f1127
broccoli88 <kiai@Deus-Ex-Machina.local>
2022-09-26 21:33:40 +0200

    redone logo / menu icon - animation / info corner icon / basic nav layout

---
+++ script.js
@@ -0,0 +1,12 @@
+// Menu icon shape shift
+
+const menu = document.querySelector(".menu");
+const lineOne = document.querySelector(".b-1");
+const lineTwo = document.querySelector(".b-2");
+const lineThree = document.querySelector(".b-3");
+
+menu.addEventListener("click", () => {
+    lineOne.classList.toggle("line-1-shift");
+    lineTwo.classList.toggle("line-2-shift");
+    lineThree.classList.toggle("line-3-shift");
+});
