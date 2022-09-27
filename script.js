b42f2b7e312770c9f386b1ac9826c28f23e315fa
broccoli88 <kiai@Deus-Ex-Machina.local>
2022-09-27 21:38:24 +0200

    Nav main / sub / sub sub category added / set style for navbaar + layout

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const menu = document.querySelector(".menu");
 const lineOne = document.querySelector(".b-1");
 const lineTwo = document.querySelector(".b-2");
 const lineThree = document.querySelector(".b-3");
+const nav = document.querySelector("nav");
 
 menu.addEventListener("click", () => {
     lineOne.classList.toggle("line-1-shift");
     lineTwo.classList.toggle("line-2-shift");
     lineThree.classList.toggle("line-3-shift");
+
+    nav.classList.toggle("nav-on");
 });
