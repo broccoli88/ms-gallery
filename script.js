77368bc3fcd0386d4af038e49f25baf5192d5b8d
broccoli88 <kiai@Deus-Ex-Machina.local>
2022-11-04 20:41:27 +0100

    Minor fixes / portfolio view / intersection obsesrver for hero

--- script.js
+++ script.js
@@ -32,28 +32,51 @@ window.addEventListener("resize", () => {
 
 // !HERO
 
-const word = document.querySelector(".text-bold");
-const wordArray = [
-    "adventure",
-    "design",
-    "animation",
-    "creation",
-    "mix",
-    "PiCTURE",
-];
+// const word = document.querySelector(".text-bold");
+// const wordArray = [
+//     "adventure",
+//     "design",
+//     "animation",
+//     "creation",
+//     "mix",
+//     "PiCTURE",
+// ];
 
-let count = 0;
+// let count = 0;
 
-function swapWords() {
-    count++;
+// function swapWords() {
+//     count++;
 
-    if (count >= wordArray.length) {
-        count = 0;
-    }
+//     if (count >= wordArray.length) {
+//         count = 0;
+//     }
 
-    word.textContent = wordArray[count];
-}
+//     word.textContent = wordArray[count];
+// }
 
-setInterval(swapWords, 3000);
+// setInterval(swapWords, 3000);
 
-// ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]   !!!!!
+// ! HERO - intersection observer
+
+const hero = document.querySelector(".hero");
+
+const options = {
+    root: document.querySelector(".body"),
+    rootMargin: "0px",
+    threshold: 1.0,
+};
+
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        const fog = document.querySelector(".fog");
+        if (!entry.isIntersecting) {
+            fog.classList.add("fog-on");
+        }
+
+        if (entry.isIntersecting) {
+            fog.classList.remove("fog-on");
+        }
+    });
+}, options);
+
+observer.observe(hero);
